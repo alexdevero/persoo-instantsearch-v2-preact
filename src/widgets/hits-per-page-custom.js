080c8de53edd12e1ee267805a7a1d8ce5d7cb68e
Alex Devero <deveroalex@gmail.com>
2020-05-04 10:53:34 +0200

    Add unique classes for hitsPerPageCustom

--- src/widgets/hits-per-page-custom.js
+++ src/widgets/hits-per-page-custom.js
@@ -6,7 +6,7 @@ export const renderHitsPerPageCustom = (renderOptions, isFirstRender) => {
 
   // On first render
   if (isFirstRender) {
-    // Create element wrapper for sortByCustom widget
+    // Create element wrapper for hitsPerPageCustom widget
     hitsPerPageCustom = document.createElement('div')
 
     // select.addEventListener('change', event => {
@@ -20,27 +20,27 @@ export const renderHitsPerPageCustom = (renderOptions, isFirstRender) => {
   // Find currently selected option
   let currentRefinementLabel = items.filter(item => item.isRefined)[0].label
 
-  // Cache the sortByCustom container
+  // Cache the hitsPerPageCustom container
   hitsPerPageCustom = document.querySelector(widgetParams.container) // container.querySelector('div')
 
   // Disable dropdown if there are no results (hits)
   if (hasNoResults) hitsPerPageCustom.classList.add('persoo-custom-select--disabled')
 
   // Add default class for hitsPerPageCustom
-  hitsPerPageCustom.classList.add('persoo-custom-select')
+  hitsPerPageCustom.classList.add('persoo-custom-select', 'persoo-custom-hits-per-page-select')
 
   // Apply root class if user provided it
   if (widgetParams.cssClasses && widgetParams.cssClasses.root) hitsPerPageCustom.classList.add(widgetParams.cssClasses.root)
 
-  // Construct DOM of sortByCustom dropdown
+  // Construct DOM of hitsPerPageCustom dropdown
   hitsPerPageCustom.innerHTML = `
-    <button class="${widgetParams.cssClasses && widgetParams.cssClasses.toggler ? widgetParams.cssClasses.toggler + ' ' : ''}persoo-custom-select-toggler">${currentRefinementLabel}</button>
-    <div class="${widgetParams.cssClasses && widgetParams.cssClasses.select ? widgetParams.cssClasses.select + ' ' : ''}persoo-custom-select-menu">
+    <button class="${widgetParams.cssClasses && widgetParams.cssClasses.toggler ? widgetParams.cssClasses.toggler + ' ' : ''}persoo-custom-select-toggler persoo-custom-hits-per-page-select-toggler">${currentRefinementLabel}</button>
+    <div class="${widgetParams.cssClasses && widgetParams.cssClasses.select ? widgetParams.cssClasses.select + ' ' : ''}persoo-custom-select-menu persoo-custom-hits-per-page-select-menu">
       ${items
       .map(
         item => `
           <a
-            class="${widgetParams.cssClasses && widgetParams.cssClasses.item ? widgetParams.cssClasses.item + ' ' : ''}persoo-custom-select-option"
+            class="${widgetParams.cssClasses && widgetParams.cssClasses.item ? widgetParams.cssClasses.item + ' ' : ''}persoo-custom-select-option persoo-custom-hits-per-page-select-option"
             data-persoo-option-value="${item.value}"
           >
             ${item.label}
@@ -52,26 +52,26 @@ export const renderHitsPerPageCustom = (renderOptions, isFirstRender) => {
 
   // Add event listener to open dropdown
   // when user clicks on dropdown button
-  hitsPerPageCustom.querySelector('.persoo-custom-select-toggler').addEventListener('click', () => {
-    hitsPerPageCustom.querySelector('.persoo-custom-select-menu').classList.toggle('persoo-custom-select-menu--visible')
+  hitsPerPageCustom.querySelector('.persoo-custom-hits-per-page-select-toggler').addEventListener('click', () => {
+    hitsPerPageCustom.querySelector('.persoo-custom-hits-per-page-select-menu').classList.toggle('persoo-custom-select-menu--visible')
   })
 
   // Add event listener to close dropdown
   // when user clicks outside it
   document.addEventListener('click', (event) => {
-    if (!event.target.classList.contains('persoo-custom-select-option') &&
-    !event.target.classList.contains('persoo-custom-select-menu') &&
-    !event.target.classList.contains('persoo-custom-select-toggler') &&
-    !event.target.classList.contains('persoo-custom-select')) {
-      if (hitsPerPageCustom.querySelector('.persoo-custom-select-menu').classList.contains('persoo-custom-select-menu--visible')) {
-        hitsPerPageCustom.querySelector('.persoo-custom-select-menu').classList.remove('persoo-custom-select-menu--visible')
+    if (!event.target.classList.contains('persoo-custom-hits-per-page-select-option') &&
+    !event.target.classList.contains('persoo-custom-hits-per-page-select-menu') &&
+    !event.target.classList.contains('persoo-custom-hits-per-page-select-toggler') &&
+    !event.target.classList.contains('persoo-custom-hits-per-page-select')) {
+      if (hitsPerPageCustom.querySelector('.persoo-custom-hits-per-page-select-menu').classList.contains('persoo-custom-select-menu--visible')) {
+        hitsPerPageCustom.querySelector('.persoo-custom-hits-per-page-select-menu').classList.remove('persoo-custom-select-menu--visible')
       }
     }
   })
 
   // Add event listener to options
   // to trigger refinement of values
-  hitsPerPageCustom.querySelectorAll('.persoo-custom-select-option').forEach(element => {
+  hitsPerPageCustom.querySelectorAll('.persoo-custom-hits-per-page-select-option').forEach(element => {
     element.addEventListener('click', event => {
       refine(event.target.dataset.persooOptionValue)
 
